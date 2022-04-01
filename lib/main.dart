164977b7c2729bc455e5255b1cd7b290fba4ce38
Ali Temel 0291000367 <ali.temel@netaszm.com.tr>
2022-04-01 11:09:41 +0300

    this is the way

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import './screens/products_overview_screen.dart';
+import './screens/product_detail_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MyShop',
+      theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple)
+              .copyWith(secondary: Colors.deepOrange),
+          fontFamily: 'Lato'),
+      home: ProductsOverviewScreen(),
+      routes: {ProductDetailScreen.routeName: (ctx) => ProductDetailScreen()},
+    );
+  }
+}
